92e0188a9ac46026c85e8a6e7f2f0911ff557af7
Jenny Knuth <inkboco@gmail.com>
2015-09-09 17:10:56 -0600

    cupcakes connected

--- app/controllers/cupcakes_controller.rb
+++ app/controllers/cupcakes_controller.rb
@@ -12,10 +12,14 @@ class CupcakesController < ApplicationController
   end
 
   def maple_bacon
+    @size = ['tiny', 'large', 'giantic']
 
   end
 
   def red_velvet
-
+    @ingredients = {
+      'red flour': '1 bag',
+      velvet: '1 yard'
+    }
   end
 end
